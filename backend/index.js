4087bc657b062ac54a7b4114c889ffa40f260725
Parth <kanhamahajan73@gmail.com>
2026-08-24 12:45:16 +0530

    added supabase in the code

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Server is running"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+    
+})
---
+++ backend/src/config/supabase.js
@@ -0,0 +1,23 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY;
+
+if (!supabaseUrl || !supabaseSecretKey) {
+  throw new Error("Supabase environment variables are missing");
+}
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseSecretKey,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+      detectSessionInUrl: false,
+    },
+  }
+);
